8d86d4f3a6d392d3cca27cc9b24b7a757629ec93
mydle19 <19mydle@gmail.com>
2022-10-26 19:31:15 -0400

    changes

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -163,15 +163,10 @@ public class BrowserUtils {
             return true;
         }
     }
-
     public static void switchToNewWindow(WebDriver driver){
         for(String each: driver.getWindowHandles()){
             if (!each.equals(driver.getWindowHandle()))
                 driver.switchTo().window(each);
         }
     }
-
-
-
-
 }
