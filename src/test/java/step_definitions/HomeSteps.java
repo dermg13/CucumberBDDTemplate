24f17eb2e44aabd91a93417b4520066c5cd81106
sugar <normklaod@gmail.com>
2022-10-27 00:57:33 -0400

    US005

--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -4,10 +4,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import pages.CommonPage;
 import pages.HomePage;
 import utils.BrowserUtils;
 
-public class HomeSteps {
+public class HomeSteps implements CommonPage {
     HomePage page;
 
     public HomeSteps(){
@@ -17,10 +19,6 @@ public class HomeSteps {
     @Given("User opens url of homepage")
     public void user_opens_url_of_homepage() {
         BrowserUtils.getDriver();
-    }
-    @Then("Verify title of page")
-    public void verify_title_of_page() {
-        BrowserUtils.assertEquals(BrowserUtils.getDriver().getTitle(), "My Store");
     }
 
     @When("User clicks on search field")
@@ -37,4 +35,20 @@ public class HomeSteps {
     public void verifySearchButtonIsEnabled() {
         BrowserUtils.assertTrue(BrowserUtils.isEnabled(page.searchBtn));
     }
+
+
+    @When("User clicks on {string} button")
+    public void userClicksOnButton(String seleniumBtn) {
+        BrowserUtils.click(BrowserUtils.getDriver().findElement
+                (By.xpath(String.format(XPATH_TEMPLATE_LINKTEXT, seleniumBtn ))
+
+        ));
+    }
+
+    @Then("Verify title of page is {string}")
+    public void verifyTitleOfPageIs(String title){
+        BrowserUtils.assertEquals(BrowserUtils.getDriver().getTitle(), title);
+    }
 }
+
+
--- src/test/resources/features/home.feature
+++ src/test/resources/features/home.feature
@@ -3,7 +3,7 @@ Feature: homepage related scenarios
   @US001
   Scenario: Verify title of page
     Given User opens url of homepage
-    Then Verify title of page
+    Then Verify title of page is "My Store"
 
 
     @US002
@@ -12,3 +12,12 @@ Feature: homepage related scenarios
       When User clicks on search field
       And User enters something in field
       Then Verify search button is enabled
+
+      #US003 Verify Selenium Framework button takes you to new page
+
+  @US003
+      Scenario: Verify Selenium Framework button
+        Given User opens url of homepage
+        When User clicks on "Selenium Framework" button
+        Then Verify title of page is "Selenium Framework | Selenium, Cucumber, Ruby, Java et al."
+
